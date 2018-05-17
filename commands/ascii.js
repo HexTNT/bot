84b19547c8429cb69d284651884b4202c4623a58
Glitch (xenonn) <none>
2018-05-17 16:33:24 +0000

    🌗🚂 Updated with Glitch

--- commands/ascii.js
+++ commands/ascii.js
@@ -2,7 +2,7 @@ const figlet = require('figlet');
 
 exports.run = async (client, message, args, tools) => {
   
-  if(args.join(' ').length > 14) return message.channel.send('Maxuimul este de 14 caractere') 
+  if(args.join(' ').length > 14) return message.channel.send('Maximul este de 14 caractere') 
   if (!args.join(' ')) return message.channel.send('Pune un test! Foloseste: mp!ascii <text>').then(msg => msg.delete({timeout: 10000})); 
     figlet(args.join(' '), (err, data) => {
       message.channel.send('```' + data + '```')
